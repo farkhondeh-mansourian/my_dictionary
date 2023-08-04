a6519eee5a9f138e2f268a089d2ea13f0b3c45ae
farkhondeh-mansourian <farkhondehmansourian@gmail.com>
2023-08-04 11:08:26 +0330

    This is my first commit :)

---
+++ main.py
@@ -0,0 +1,62 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+#if __name__ == '__main__':
+    #print_hi('PyCharm')
+
+
+
+
+
+word1 = {
+  "word": "english",
+  "definition": "It's a language",
+  "meaning in Farsi": "انگلیسی",
+  "example": "I speak English."
+}
+
+word2 = {
+  "word": "declare",
+  "definition": "",
+  "meaning in Farsi": "اعلام کردن",
+  "example": ""
+}
+
+word3 = {
+  "word": "proximity",
+  "definition": "",
+  "meaning in Farsi": "نزدیکی",
+  "example": ""
+}
+
+word4 = {
+  "word": "obstacle",
+  "definition": "",
+  "meaning in Farsi": "مانع",
+  "example": ""
+}
+
+word5 = {
+  "word": "quotation mark",
+  "definition": "",
+  "meaning in Farsi": "علامت نقل قول",
+  "example": ""
+}
+
+my_dictionary = {
+  "english": word1,
+  "declare": word2,
+  "proximity": word3,
+  "obstacle": word4,
+  "quotation mark": word5
+}
+
+print(my_dictionary["proximity"])
+print(my_dictionary["proximity"]["meaning in Farsi"])
